57baf0e9c3ac0ce4101b9e72e44863d95e3995c1
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-05-14 18:07:23 +0300

    Operators

---
+++ Operators/Arithmetic Operators/index.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// // exponentiation x to the power of y
+// console.log(x ** y);
+
+// Increment (++)
+console.log(x++);
+console.log(x);
+
+// Decrement (--)
+console.log(--x);
