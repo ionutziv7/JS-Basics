a88ccb73ef3d14f72e0968e284244cbed028e19a
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-05-21 21:08:28 +0300

    Removing Elements

---
+++ Arrays/Removing Elements.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4];
+
+// End
+const last = numbers.pop();
+console.log(numbers);
+console.log(last);
+
+// Beginning
+numbers.unshift();
+
+// Middle
+numbers.splice(2, 1);
+console.log(numbers);
